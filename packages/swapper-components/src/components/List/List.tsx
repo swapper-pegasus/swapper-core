4226873ca29a522cc1ac9b31e74bfe19e09b05f6
hippocampus <hippocampus@swapper.market>
2021-12-28 12:53:33 +0100

    Enable test swapper-components and add to CI

--- packages/swapper-components/src/components/List/List.tsx
+++ packages/swapper-components/src/components/List/List.tsx
@@ -5,7 +5,7 @@ type Header = {
   parts: number, // Total 12 parts
 }
 
-type Props = {
+export type Props = {
   headers: Array<Header>,
   rows: Array<Array<string | JSX.Element>>
 }
@@ -14,32 +14,19 @@ export function List ({ headers, rows }:Props) {
   return <div className='w-full'>
     <div className='flex'>
       {
-        headers.map((header) => {
-          return <div className={`w-${header.parts}/12 border-b`}><p className='font-light text-swpGray-light p-2'>{header.name}</p></div>
+        headers.map((header, index) => {
+          return <div key={index} className={`w-${header.parts}/12 border-b`}><p className='font-light text-swpGray-light p-2'>{header.name}</p></div>
         })
       }
     </div>
     <div>
-      {/* {
-        headers.map((header, index) => {
-          return (
-            <div className='flex w-full'>
-              {
-                rows.map((row) => {
-                  return <div className={`w-${header.parts}/12 border-b p-2`}>{row[index]}</div>
-                })
-              }
-            </div>
-          )
-        })
-      } */}
       {
-        rows.map((row) => {
+        rows.map((row, index) => {
           return (
-            <div className='flex w-full'>
+            <div key={index} className='flex w-full'>
               {
                 row.map((element, index) => {
-                  return <div className={`w-${headers[index].parts}/12 border-b p-2`}>{element}</div>
+                  return <div key={index} className={`w-${headers[index].parts}/12 border-b p-2`}>{element}</div>
                 })
               }
             </div>
