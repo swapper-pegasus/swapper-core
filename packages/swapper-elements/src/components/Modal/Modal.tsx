4b64d4c90159dbd55c441b772f38952a937d8402
hippocampus <hippocampus@swapper.market>
2021-12-31 13:21:04 +0100

    Refactor Modal element

--- packages/swapper-elements/src/components/Modal/Modal.tsx
+++ packages/swapper-elements/src/components/Modal/Modal.tsx
@@ -3,14 +3,12 @@ import ReactDOM from 'react-dom'
 import { Button } from '../Button'
 
 type Props = {
-  title?: string,
-  description: JSX.Element | string,
-  icon?: JSX.Element | string,
+  children?: JSX.Element | string;
   isOpen: boolean,
   onClose: () => void;
 };
 
-export function Modal ({ title, icon, description, isOpen, onClose }: Props) {
+export function Modal ({ children, isOpen, onClose }: Props) {
   const modalContainer = document.querySelector('#modalContainer')
 
   if (!isOpen || !modalContainer) {
@@ -25,28 +23,9 @@ export function Modal ({ title, icon, description, isOpen, onClose }: Props) {
               <span className="hidden" aria-hidden="true">&#8203;</span>
               <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all p-6">
                 <div className="bg-white">
-                  <div className="flex items-start">
-                    <div className="mt-3">
-                      {
-                        title && <h3 className="text-lg leading-6 font-medium text-gray-900" id="modal-title">
-                        {title}
-                      </h3>
-                      }
-                      <div className="flex mt-2">
-                        {
-                          icon &&
-                          <div className='px-2'>
-                            { icon }
-                          </div>
-                        }
-                        <div className="text-sm text-swpGray">
-                          {description}
-                        </div>
-                      </div>
-                    </div>
-                  </div>
+                  {children}
                 </div>
-                <div className="bg-white pt-6 py-3 text-right">
+                <div className="bg-white pt-3 py-3 text-right">
                   <Button dataTestId='modal-accept-button' onClick={() => onClose()}>
                     <span>Continuar</span>
                   </Button>
