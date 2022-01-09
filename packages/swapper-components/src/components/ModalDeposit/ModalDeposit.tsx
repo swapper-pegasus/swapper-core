61c017c7507426ea8a54e4506806cf171957075c
hippocampus <hippocampus@swapper.market>
2022-01-09 09:46:10 +0100

    Fix coverage

--- packages/swapper-components/src/components/ModalDeposit/ModalDeposit.tsx
+++ packages/swapper-components/src/components/ModalDeposit/ModalDeposit.tsx
@@ -9,14 +9,20 @@ type Props = {
   onClose: () => void;
 };
 
-export function ModalDeposit ({ token, address, isOpen, onClose }: Props) {
+const DEFAULT_TOKEN = {
+  id: '',
+  name: '',
+  symbol: ''
+}
+
+export function ModalDeposit ({ token = DEFAULT_TOKEN, address, isOpen, onClose }: Props) {
   return (
     <Modal isOpen={isOpen} onClose={() => onClose()}>
       <div data-testid='deposit-modal'>
         <div className="flex flex-col justify-center items-center">
           <Alert
             type='warning'
-            description={`Sending any crypto asset besides ${token?.name} to this address may result in the total loss of your funds`}
+            description={`Sending any crypto asset besides ${token.name} to this address may result in the total loss of your funds`}
           />
           <div className="mt-3">
             <p className='font-medium'>{address}</p>
@@ -30,4 +36,8 @@ export function ModalDeposit ({ token, address, isOpen, onClose }: Props) {
       </div>
     </Modal>
   )
+}
+
+ModalDeposit.defaultProps = {
+  token: DEFAULT_TOKEN
 }
